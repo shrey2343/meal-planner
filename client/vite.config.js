eabba181f954aae0314157aa50b82c2a04726b34
shrey2343 <shreyn82@gmail.com>
2025-12-24 20:38:01 +0530

    added vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
-  // vite.config.js
-
-  // ...
-  
+  publicDir: 'public',
 })
